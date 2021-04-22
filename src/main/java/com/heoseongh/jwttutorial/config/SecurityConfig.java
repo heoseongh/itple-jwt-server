51df973e89737e85dd04aff273415811dd46115c
heoseongh <shheo0807@gmail.com>
2021-04-23 00:05:17 +0900

    Update: SecurityConfig 각종 설정 추가

--- src/main/java/com/heoseongh/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/heoseongh/jwttutorial/config/SecurityConfig.java
@@ -1,21 +1,67 @@
 package com.heoseongh.jwttutorial.config;
 
+import com.heoseongh.jwttutorial.jwt.JwtAccessDeniedHandler;
+import com.heoseongh.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import com.heoseongh.jwttutorial.jwt.JwtSecurityConfig;
+import com.heoseongh.jwttutorial.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // 토큰 방식을 사용하기 때문에 csrf 설정은 필요 없다.
+                .csrf().disable()
+
+                // 구현한 예외처리 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // sameOrigin(같은 도메인) 상에서만 iframe 접근이 가능하도록 X-Frame-Options 설정.
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않도록 설정(STATELESS)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 로그인, 회원가입 API는 토큰이 없는 않은 상태에서 요청이 들어오기 때문에 열어준다.
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated()
+
+                // JwtSecurityConfig 클래스 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 
     @Override
