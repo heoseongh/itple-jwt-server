eed6074e799bfb9631b7ba66f5ff427b9589b5ef
heoseongh <shheo0807@gmail.com>
2021-04-23 00:00:51 +0900

    Add: 인증/인가 예외 클래스 구현

---
+++ src/main/java/com/heoseongh/jwttutorial/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.heoseongh.jwttutorial.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // 인증과정에서 실패하거나 인증헤더(Authorization)를 보내지 않게되는 경우
+    // 401(UnAuthorized) 메시지 처리
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
